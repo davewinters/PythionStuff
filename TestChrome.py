489d82e601e831b1a414c57208dbb53f6a7d796d
davewinters <wntrsdv@yahoo.com>
2019-10-09 22:33:30 +0100

    Chrome headless test2

--- TestChrome.py
+++ TestChrome.py
@@ -1,15 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import os
+user = ""
+pwd = ""
+#driver = webdriver.Chrome()
 
-# instantiate a chrome options object so you can set the size and headless preference
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 
-# go to Google and click the I'm Feeling Lucky button
-driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
-driver.get("https://www.google.com")
-lucky_button = driver.find_element_by_css_selector("[name=btnI]")
-lucky_button.click()
 
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get("http://www.facebook.com")
+assert "Facebook" in driver.title
+#elem = driver.find_element_by_id("email")
+#elem.send_keys(user)
+#elem = driver.find_element_by_id("pass")
+#elem.send_keys(pwd)
+#elem.send_keys(Keys.RETURN)
+
+driver.close()
