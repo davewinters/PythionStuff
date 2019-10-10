2e2a763c7c5d0d562009da1b15b58bde34188b5c
davewinters <wntrsdv@yahoo.com>
2019-10-10 15:14:50 +0100

    Chrome headless test3

--- TestChrome.py
+++ TestChrome.py
@@ -9,7 +9,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 
 
-driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome("/home/david/.jenkins/tools/chromedriver/")
 driver.get("http://www.facebook.com")
 assert "Facebook" in driver.title
 #elem = driver.find_element_by_id("email")
